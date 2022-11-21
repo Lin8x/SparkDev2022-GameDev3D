0ec45fb178bf6e2725f8abef64071dd69c77bbae
Ricardo Garcia <rickyg0731@yahoo.com>
2022-11-20 20:36:24 -0500

    Big Update
    Big Update Gameplay

---
+++ Assets/DEP_MECHANIC/Ricky's Stuff/Scripts/enemy_spawner.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemy_spawner : MonoBehaviour
+{
+
+    public GameObject enemy_object;
+    public GameObject enemy_reset_position;
+    public GameObject player;
+    public NavMeshAgent enemy_agent;
+    public Animator slender_animator;
+    public float enemy_health = 100;
+    public GameObject blood_ui;
+    public GameObject slender_glitch;
+    public player_script playerscript;
+    public float damage_amount = 2;
+    public AudioSource hit_sound_2;
+    public AudioSource growl_sound;
+    public GameObject action_music;
+    public disable_item action_music_disable_component;
+
+    bool start_reset_timer = false;
+    float reset_timer = 5f;
+    public bool following_playuer = false;
+
+    public AudioSource hit_sound;
+
+    public void take_damage(float damage)
+    {
+        enemy_health = enemy_health - damage;
+        hit_sound.Play();
+        hit_sound_2.Play();
+    }
+
+    void Update()
+    {
+        //RESPAWN TIMER
+        if(start_reset_timer == true)
+        {
+            reset_timer = reset_timer - Time.deltaTime;
+            if (reset_timer <= 0)
+            {
+                enemy_health = 100;
+                enemy_object.SetActive(true);
+                start_reset_timer = false;
+                reset_timer = 5;
+            }
+        }
+
+        //RESPAWN ENEMY
+        if(enemy_health <= 0 && start_reset_timer == false)
+        {
+            enemy_object.SetActive(false);
+            enemy_object.transform.position = enemy_reset_position.transform.position;
+            start_reset_timer = true;
+        }
+    
+        if (Vector3.Distance(enemy_object.transform.position, player.transform.position) < 2)
+        {
+            //ATTACK PLAYER
+            blood_ui.SetActive(true);
+            slender_glitch.SetActive(true);
+            slender_animator.SetInteger("state", 2);
+            playerscript.health = playerscript.health - Time.deltaTime * damage_amount;
+            //enemy_agent.SetDestination(this.transform.position);                       
+        }
+        if (Vector3.Distance(enemy_object.transform.position, player.transform.position) > 3 && Vector3.Distance(enemy_object.transform.position, player.transform.position) < 20)
+        {
+            //FOLLOW PLAYER
+            if (following_playuer == false)
+            {
+                growl_sound.Play();
+                following_playuer = true;
+            }
+            action_music_disable_component.timer_to_disable = 2;
+            action_music.SetActive(true);
+            enemy_object.transform.LookAt(player.transform.position);
+            slender_animator.SetInteger("state", 1);
+            enemy_agent.SetDestination(player.transform.position);                      
+        }
+        if (Vector3.Distance(enemy_object.transform.position, player.transform.position) > 20)
+        {
+            following_playuer = false;         
+            //RANDOM LOCATION
+            // enemy_agent.SetDestination(this.transform.position);                  
+        }
+    }
+
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        slender_animator.SetLookAtPosition(player.transform.position);
+        slender_animator.SetLookAtWeight(1);
+    }
+
+
+}
---
+++ Assets/DEP_MECHANIC/Ricky's Stuff/Scripts/guns_script.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guns_script : MonoBehaviour
+{
+
+    public GameObject shotgun;
+    public GameObject pistol;
+    public GameObject shotgun_bullet;
+    public GameObject pistol_bullet;
+    public GameObject shotgun_bullet_location;
+    public GameObject pistol_bullet_location;
+
+    public float bullet_force = 250;
+
+    public AudioSource shotgunsound_1;
+    public AudioSource shotgunsound_2;
+    public AudioSource pistol_sound_1;
+    public AudioSource pistol_sound_2;
+    public AudioSource reload_sound;
+    public AudioSource switch_gun_sound;
+
+    public Animator shotgun_animator;
+    public Animator pistol_animator;
+
+    public ParticleSystem shotgun_particle;
+    public ParticleSystem pistol_particle;
+
+    bool start_fire_rate = false;
+    float fire_Rate = 0.8f;
+    float fire_rate_reset = 0.8f;
+
+    void Update()
+    {
+
+        if(start_fire_rate == true)
+        {
+            fire_Rate = fire_Rate - Time.deltaTime;
+            if (fire_Rate <= 0)
+            {
+                if(shotgun.activeInHierarchy == true)
+                {
+                    shotgun_animator.SetInteger("state", 0);
+                }
+                if (pistol.activeInHierarchy == true)
+                {
+                    pistol_animator.SetInteger("state", 0);
+                }              
+                start_fire_rate = false;
+                fire_Rate = fire_rate_reset;
+            }
+        }
+
+
+        if (Input.GetKeyDown("1"))
+        {
+            switch_gun_sound.Play();
+            if (shotgun.activeInHierarchy == true)
+            {
+                shotgun.SetActive(false);
+            }
+            else if (shotgun.activeInHierarchy == false)
+            {
+                shotgun.SetActive(true);
+                pistol.SetActive(false);
+            }  
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            switch_gun_sound.Play();
+            if (pistol.activeInHierarchy == true)
+            {
+                pistol.SetActive(false);
+            }
+            else if (pistol.activeInHierarchy == false)
+            {
+                shotgun.SetActive(false);
+                pistol.SetActive(true);
+            }
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && start_fire_rate == false)
+        {
+            shotgun_animator.speed = 2.2f;
+            fire_rate_reset = 0.35f;         
+            if (shotgun.activeInHierarchy == true && shotgun_animator.GetInteger("state") != 1)
+            {         
+                int choose = Random.Range(0, 2);
+                if(choose == 0)
+                {
+                    shotgunsound_1.Play();
+                }
+                else
+                {
+                    shotgunsound_2.Play();
+                }
+                GameObject bullet = Instantiate(shotgun_bullet, shotgun_bullet_location.transform.position, shotgun_bullet_location.transform.rotation);
+                Rigidbody bullet_rig = bullet.GetComponent<Rigidbody>();
+                bullet_rig.AddForce(shotgun.transform.forward * bullet_force);
+                shotgun_particle.Play();
+                shotgun_animator.SetInteger("state", 1);
+                start_fire_rate = true;
+            }
+            if (pistol.activeInHierarchy == true && pistol_animator.GetInteger("state") != 1)
+            {
+                pistol_animator.speed = 3.5f;
+                fire_rate_reset = 0.17f;
+                int choose = Random.Range(0, 2);
+                if (choose == 0)
+                {
+                    pistol_sound_1.Play();
+                }
+                else
+                {
+                    pistol_sound_2.Play();
+                }
+                GameObject bullet = Instantiate(pistol_bullet, pistol_bullet_location.transform.position, pistol_bullet_location.transform.rotation);
+                Rigidbody bullet_rig = bullet.GetComponent<Rigidbody>();
+                bullet_rig.AddForce(shotgun.transform.forward * bullet_force);
+                pistol_particle.Play();
+                pistol_animator.SetInteger("state", 1);
+                start_fire_rate = true;
+            }
+        }
+
+
+        if (Input.GetKeyDown("r"))
+        {
+            if (shotgun.activeInHierarchy == true)
+            {
+                shotgun_animator.speed = 1.5f;
+                shotgun_animator.SetInteger("state", 2);
+            }
+
+            if (pistol.activeInHierarchy == true)
+            {
+                pistol_animator.speed = 1.5f;
+                pistol_animator.SetInteger("state", 2);
+            }
+            reload_sound.Play();
+            start_fire_rate = true;
+        }
+    }
+}
